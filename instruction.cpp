13e947be5d54b09ed315e0043cc8d86491549381
Pierre Strucviel <pierrestrucviel@wifiroam060084.univ-st-etienne.fr>
2026-04-27 09:44:11 +0200

    baguette

--- instruction.cpp
+++ instruction.cpp
@@ -9,4 +9,11 @@ Command::Commande(instructions){
 
 void Commande::ajouter_instruction(instruction* instr) {
     liste_instructions_.push_back(instr);
-})
+};
+
+void Commande::execute(Tortue& t) {
+    for (auto instr : liste_instructions_) {
+        instr->execute(t);
+    }
+}
+
